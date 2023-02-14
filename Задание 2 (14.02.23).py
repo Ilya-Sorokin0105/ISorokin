e9fa44a4b8b1700f4cc576e9c22b1f1e95b0dbec
Ilya-Sorokin0105 <117991102+Ilya-Sorokin0105@users.noreply.github.com>
2023-02-14 10:06:02 +0300

    Классная работа (14.02.23)

---
+++ Задание 2 (14.02.23).py
@@ -0,0 +1,17 @@
+a = 23
+b = 12
+# 2 задание
+print(a>20 and b<13)
+print(a>17 and b >7)
+print(a>24 and b<13)
+print(a>20 and b>20)
+# 3 задание
+print(a>20 or b<10)
+print(a>21 or b>17)
+print(a>24 or b<7)
+print(a>29 or b>20)
+# 4 задание
+x = "1234"
+y = "4321"
+print (x!=y)
+print (x==y)
