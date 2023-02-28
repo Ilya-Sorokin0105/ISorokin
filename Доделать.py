f045f1dab6029d9afadaffaa72fa74c1e42dc870
Ilya-Sorokin0105 <117991102+Ilya-Sorokin0105@users.noreply.github.com>
2023-02-28 10:14:29 +0300

    Доделать

---
+++ Доделать.py
@@ -0,0 +1,27 @@
+#1 
+n = (input())
+if (int(n)>999) and (int(n)<10000):
+    a = int(n[0])+int(n[1])+int(n[2])+int(n[3])
+    b = int(n[0])*int(n[1])*int(n[2])*int(n[3])
+    print (a)
+    print (b)
+else:
+    print("не четырехзначное число")
+#2 
+a = (input())
+if (int(a)>9999) and (int(a)<100000):
+    q = int(a[3]) 
+    w = int(a[4])
+    print (q)
+    print (w)
+else:
+    print("не пятизначное число")
+#3
+
+#5
+
+#6
+
+#7
+
+#8
